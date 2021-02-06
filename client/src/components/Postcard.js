8d083c2f11cd2ad496d94d155114992b082efbb0
남송녕 <62981041+Songnyeong-Nam@users.noreply.github.com>
2021-02-06 16:24:13 +0900

    0206 getPosts

---
+++ client/src/components/Postcard.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+import { Card, Icon, Label, Button, Image } from 'semantic-ui-react'
+
+
+const Postcard = ({ post: { body, createdAt, id, username, likes } }) => {
+  const timestamp = moment(createdAt).fromNow()
+  
+  function likePost() {
+
+  }
+
+  return (
+    <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/jenny.jpg'
+        />
+        <Card.Header>{username}</Card.Header>
+        <Link to={`/posts/${id}`}>
+          <Card.Meta>{timestamp}</Card.Meta>
+          <Card.Description>
+            {body}
+          </Card.Description>
+        </Link>
+      </Card.Content>
+      <Card.Content extra>
+        <Button as='div' labelPosition='right' onClick={likePost} >
+          <Button color='red' size='mini'>
+            <Icon name='heart' />
+              Like
+            </Button>
+          <Label as='a' basic color='red' pointing='left'>
+            00
+      </Label>
+        </Button>
+        <Button as='div' labelPosition='right' onClick={likePost} >
+          <Button color='red' size='mini'>
+            <Icon name='comment' />
+              comments
+            </Button>
+          <Label as='a' basic color='red' pointing='left'>
+            00
+      </Label>
+        </Button>
+      </Card.Content>
+    </Card>
+  )
+
+};
+
+export default Postcard;
--- client/src/router/Home.js
+++ client/src/router/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 
-import { Grid, Breadcrumb } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
+import PostCard from '../components/Postcard'
 
 const FETCH_POSTS = gql`
   {
@@ -29,27 +30,26 @@ const FETCH_POSTS = gql`
 const Home = () => {
   const {
     loading,
-    data: { getPosts },
+    data,
   } = useQuery(FETCH_POSTS);
 
   return (
     <>
       <Grid columns={3} divided>
-        {loading && <h1>Loading...</h1>}
-        {getPosts && 
-          <>
-            <Grid.Row>
-              <h1> recent posts </h1>
-            </Grid.Row>
-            <Grid.Row>
-              {getPosts.map((post) => (
-                <Grid.Column key={post.id}>
-                  <Breadcrumb post={post} />
-                </Grid.Column>
-              ))}
-            </Grid.Row>
-          </>
-        }
+        <Grid.Row>
+          <h1> recent posts </h1>
+        </Grid.Row>
+        <Grid.Row>
+          {loading && <h1>Loading...</h1>}
+          {data && data.getPosts && 
+          data.getPosts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post}/>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+
+
       </Grid>
     </>
   );
